29fa7867b867d6eec9b581f2269cec69bbdd5387
Digilex <digilex10@gmail.com>
2025-09-10 19:02:51 +0500

    first commits

--- App.js
+++ App.js
@@ -3,8 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Homescreen from './screens/HomeScreen';
 import GameScreen from './screens/GameScreen';
-// import AboutScreen from './screens/AboutScreen';
-// import SettingsScreen from './screens/SettingsScreen';
 import AdvancedScreen from './screens/AdvancedScreen';
 import BeginnerScreen from './screens/BeginnerScreen';
 import IntermediateScreen from './screens/IntermediateScreen';
@@ -21,16 +19,9 @@ export default function App() {
           headerShown: false
         }}
       >
-          {/* <Stack.Screen name="GameScreen" component={GameScreen} />
-        
-        <Stack.Screen name="IntermediateScreen" component={DummyScreen} />
-        <Stack.Screen name="AdvancedScreen" component={DummyScreen} />
-        <Stack.Screen name="ExpertScreen" component={DummyScreen} /> */}
-        
+          
         <Stack.Screen name="Home" component={Homescreen} />
         <Stack.Screen name="Game" component={GameScreen} />
-        {/* <Stack.Screen name="Settings" component={SettingsScreen} />
-        <Stack.Screen name="About" component={AboutScreen} /> */}
         <Stack.Screen name="AdvancedScreen" component={AdvancedScreen} />
         <Stack.Screen name="BeginnerScreen" component={BeginnerScreen} />
         <Stack.Screen name="IntermediateScreen" component={IntermediateScreen} />
